6cd968984ad9880e2b9e17218ab14bf0d9abf9ca
Dmytro Ihnatushko <drimasik.ig@gmail.com>
2026-02-10 16:37:31 +0200

    second map

--- maps/maps.go
+++ maps/maps.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	users := map[string]int{
-		"Vasya":  15,
-		"Petya":  23,
-		"Kostya": 45,
-	}
+	// users := map[string]int{
+	// 	"Vasya":  15,
+	// 	"Petya":  23,
+	// 	"Kostya": 45,
+	// }
+
+	var users map[string]int
+	users = make(map[string]int)
+
+	users["Petya"] = 19
+	users["Kostya"] = 44
+
+	delete(users, "Petya")
 
 	age, exists := users["Kostya"]
 
@@ -18,4 +26,17 @@ func main() {
 	} else {
 		fmt.Println("not on the list")
 	}
+
+	for key, value := range users {
+		 fmt.Println(key, value)
+	}
+		fmt.Println(len(users))
+
+	first := []int{1, 2, 3}
+	second := first 
+
+	first = append(first, 4)
+
+	fmt.Println(&first[0])
+	fmt.Println(&second[0])
 }
