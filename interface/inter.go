071302de50412e24cb34eab248cb41e0d65ae741
Dmytro Ihnatushko <drimasik.ig@gmail.com>
2026-02-17 11:10:30 +0200

    learn intefaces

--- interface/inter.go
+++ interface/inter.go
@@ -5,8 +5,17 @@ import (
 	"math"
 )
 
-type Shape interface {
+type ShapeWithArea interface {
 	Area() float32
+}
+
+type ShapeWithPerimeter interface {
+	Perimetr() float32
+}
+
+type Shape interface {
+	ShapeWithArea
+	ShapeWithPerimeter
 }
 
 type Square struct {
@@ -15,6 +24,10 @@ type Square struct {
 
 func (s Square) Area() float32 {
 	return s.sideLenght * s.sideLenght
+}
+
+func (s Square) Perimetr() float32 {
+	return s.sideLenght * 4
 }
 
 type Circle struct {
@@ -27,12 +40,39 @@ func (c Circle) Area() float32 {
 
 func main() {
 	square := Square{5}
-	circle := Circle{7}
+	// circle := Circle{7}
 
 	PrintShapeArea(square)
-	PrintShapeArea(circle)
+	// PrintShapeArea(circle)
+
+	// printInterface(square)
+	// printInterface(circle)
+	printInterface("sada")
+	printInterface(77)
+	printInterface("ggtdvdv")
+	// printInterface(true)
 }
 
 func PrintShapeArea(shape Shape) {
 	fmt.Println(shape.Area())
+	fmt.Println(shape.Perimetr())
+}
+
+func printInterface (i interface{}) {
+// 	switch value := i.(type) {
+// 	case int: 
+// 		fmt.Println("int", value)
+// 	case bool: 
+// 		fmt.Println("bool", value)
+// 	default: 
+// 		fmt.Println("unknown type", value)
+// 	}
+// 	fmt.Printf("%#v\n", i)
+
+	str, ok := i.(string)
+	if !ok {
+		fmt.Println("interface is not string")
+		return
+	}
+	fmt.Println(len(str))
 }
